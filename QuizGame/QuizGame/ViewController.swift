443fbdff36edf4428fe004d5d88ec12c30f31f76
areekaras <shibili.areekara@gmail.com>
2021-11-05 19:09:36 +0530

    Prototyping with storyboards

---
+++ QuizGame/QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Shibili Areekara on 05/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
